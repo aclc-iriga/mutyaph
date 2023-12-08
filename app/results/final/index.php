2a8bcbf53d9cba6e27b37a43e55a71b479942b25
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2023-12-08 10:34:20 +0800

    Display all joined candidates

--- app/results/final/index.php
+++ app/results/final/index.php
@@ -59,7 +59,7 @@
     $unique_total_rank_ave_equivs = [];
     $unique_total_percentages = [];
     $unique_adjusted_ranks = [];
-    foreach(Team::all() as $team) {
+    foreach($events[0]->getAllTeams() as $team) {
         $team_key = 'team_'.$team->getId();
         $t = [
             'info'    => $team->toArray(),
